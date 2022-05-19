22ec6fb3cab561086d77170f77e55fcd1396e006
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-05-19 22:28:33 +0200

    feat: Creating Results Page

---
+++ bar-roulette/src/pages/results.js
@@ -0,0 +1,12 @@
+import react from 'React'
+import footer from '../components/general/footer'
+import header from '../components/general/header'
+
+const ResultPage = () =>{
+    <div>
+        <header />
+
+        <footer />
+    </div>
+
+}
