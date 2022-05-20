4c75aa66413af7d7ec9a23f2a026e9ce7cef98a0
RogerRochoRibas <rogerrochoribas@gmail.com>
2022-05-20 03:16:43 +0200

    Creado un Bar Locaction como place holder

---
+++ bar-roulette/src/components/results/resultsBarLocation.js
@@ -0,0 +1,7 @@
+export function ResultsBarLocation() {
+    return <div id='resultsBarLocation'>
+        <h2>It's a Match</h2>
+        <h2>Ubicación: Plaça Reial, 10</h2>
+        <h3>Barri Gotic</h3>
+    </div>
+}
--- bar-roulette/src/pages/results.js
+++ bar-roulette/src/pages/results.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import EmailComponent from '../components/results/inputPeople';
+import React from "react";
+import EmailComponent from "../components/results/inputPeople";
+import { ResultsBarLocation } from "../components/results/resultsBarLocation";
 
 const ResultPage = () => {
-    return (
-        <div id='resultsPage'>
-            <EmailComponent />
-        </div>
-    );
-}
+  return (
+    <div id="resultsPage">
+      <ResultsBarLocation />
+      <EmailComponent />
+    </div>
+  );
+};
 
 export default ResultPage;
